09ad9f53740bfc9ca8d350549cddb51f3b606bb0
artless <jayanlas00@gmail.com>
2025-09-03 08:48:24 -0400

    strings are immutable, will need to make the string an array and work from that

--- BiscuitTower/BiscuitTower/Game1.cs
+++ BiscuitTower/BiscuitTower/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using JairLib.TileGenerators;
 
 namespace BiscuitTower
 {
@@ -8,6 +9,9 @@ namespace BiscuitTower
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private SpriteFont _font;
+        string seed;
 
         public Game1()
         {
@@ -27,6 +31,7 @@ namespace BiscuitTower
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _font = Content.Load<SpriteFont>("PrettyPixelBIG");
             // TODO: use this.Content to load your game content here
         }
 
@@ -37,15 +42,28 @@ namespace BiscuitTower
 
             // TODO: Add your update logic here
 
+            var keyb = Keyboard.GetState();
+            if (keyb.IsKeyDown(Keys.Enter))
+            {
+                seed = SeedBuilder.TheStringGetsThisLength(16);
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            if (!string.IsNullOrEmpty(seed))
+            {
+                _spriteBatch.DrawString(_font, seed, new Vector2(64, 64), Color.White);
+            }
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ BiscuitTower/JairLib/TileGenerators/SeedBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JairLib.TileGenerators
+{
+    public static class SeedBuilder
+    {
+        public static string TheStringGetsThisLength(int length)
+        {
+            var str = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                str += "0";
+            }
+
+            return str;
+        }
+
+        public static string TheSeedGetsSomeOnes(string seed)
+        {
+            var tempSeed = seed;
+
+            for (int i = 0; i <= seed.Length; i++)
+            {
+                int rand = Random.Shared.Next(0, 10);
+
+                if (rand < 5)
+                {
+                    ///IM AN IDIOT STRINGS ARE IMMUTABLE AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+                    //tempSeed.cut
+                    //tempSeed.Insert;
+                    //tempSeed[i] = rand;
+                }
+            }
+
+            return seed;
+        }
+    }
+}
